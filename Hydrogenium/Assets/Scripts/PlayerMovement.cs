453d3e784a7a5272fead2d78a3499df442f12ac4
Kyle Jones <56350720+Rusty942@users.noreply.github.com>
2024-03-30 21:06:45 +0000

    Improved movement
    Vastly improved improvement

--- Hydrogenium/Assets/Scripts/PlayerMovement.cs
+++ Hydrogenium/Assets/Scripts/PlayerMovement.cs
@@ -1,56 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    public float moveSpeed = 5f; // Movement speed of the character
-    public float jumpForce = 10f; // Force applied when jumping
-    private Animator animator; // Reference to the Animator component
-    private SpriteRenderer spriteRenderer; // Reference to the SpriteRenderer component
-    private Rigidbody2D rb; // Reference to the Rigidbody2D component
-    private BoxCollider2D boxCollider; // Reference to the BoxCollider2D component
+public class PlayerMovement : MonoBehaviour {
 
-    // Start is called before the first frame update
-    void Start()
+	public CharacterController2D controller;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+
+    private Animator animator; // Reference to the Animator component
+
+    void Start ()
     {
         // Get the Animator component attached to the same GameObject
         animator = GetComponent<Animator>();
-
-        // Get the SpriteRenderer component attached to the same GameObject
-        spriteRenderer = GetComponent<SpriteRenderer>();
-
-        // Get the Rigidbody2D component attached to the same GameObject
-        rb = GetComponent<Rigidbody2D>();
-
-        // Get the BoxCollider2D component attached to the same GameObject
-        boxCollider = GetComponent<BoxCollider2D>();
     }
+	
+	// Update is called once per frame
+	void Update () {
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Get the horizontal input (A and D keys or left and right arrow keys)
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        // Calculate the movement amount
-        float movement = horizontalInput * moveSpeed * Time.deltaTime;
-
-        // Move the character
-        transform.Translate(new Vector3(movement, 0f, 0f));
-
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         // Update the walking animation
-        UpdateWalkingAnimation(horizontalInput);
+        UpdateWalkingAnimation(horizontalMove);
 
-        // Flip the character sprite and collider if moving left
-        FlipCharacter(horizontalInput);
-
-        // Check for jump input
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetButtonDown("Jump"))
         {
-            Jump();
+            jump = true;
         }
-    }
+	}
 
-    // Function to update the walking animation based on input
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+	}
+
     private void UpdateWalkingAnimation(float horizontalInput)
     {
         // If the horizontal input is not zero, set the "Walking" parameter to true
@@ -63,32 +52,5 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetBool("Walking", false);
         }
-    }
-
-    // Function to flip the character sprite and collider based on movement direction
-    private void FlipCharacter(float horizontalInput)
-    {
-        // If moving left, flip the sprite and collider horizontally
-        if (horizontalInput < 0)
-        {
-            spriteRenderer.flipX = true;
-            boxCollider.offset = new Vector2(-boxCollider.offset.x, boxCollider.offset.y);
-        }
-        // If moving right, flip the sprite and collider back to their original orientation
-        else if (horizontalInput > 0)
-        {
-            spriteRenderer.flipX = false;
-            boxCollider.offset = new Vector2(-boxCollider.offset.x, boxCollider.offset.y);
-        }
-    }
-
-    // Function to handle player jumping
-    private void Jump()
-    {
-        // Check if the player is grounded (you may need to implement your own grounded check)
-        // For simplicity, this example assumes the player is always grounded
-        // You may need to replace this with your own grounded check
-        // e.g., using a RaycastHit2D or checking if the player is colliding with the ground
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 }
